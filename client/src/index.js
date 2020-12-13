3af54f3a71b7a790d2745ffe73f5bf6677764232
tlaloc_debug <pelondinho@hotmail.com>
2020-12-12 19:42:07 -0500

    add table format

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import react, {useState} from "react";
 import reactDOM from "react-dom";
 import axios from "axios";
+import "./index.css"
 
 function App() {
 
@@ -12,10 +13,27 @@ function App() {
         }); 
     }
 
+    const rendermicros = (micros) => {
+        return (
+            <tr>
+                <td>{micros.product}</td>
+                <td>{micros.progmemory}</td>
+            </tr>
+        )
+    }
+
     return (
         <div>
             <button onClick={search}>Search</button>
-            {List.map(micros => (<p>{micros.product}</p>))}
+            <thead>
+                <tr>
+                    <td>micro</td>
+                    <td>memory</td>
+                </tr>
+            </thead>
+            <tbody>
+                {List.map(rendermicros)}
+            </tbody>
         </div>
     );
 }
