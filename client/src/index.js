e19b0c81dfbd9993684e87c5a3f1fca7f0275d19
tlaloc_debug <pelondinho@hotmail.com>
2020-12-21 20:00:00 -0500

    add Advance_Search button

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,55 @@ import "./index.css"
 
 function App() {
 
-    const [Pic, setPic]=useState(1); 
+    const [Pic, setPic]=useState(""); 
+    const [memory, setmemory]=useState(0);
+    const [eeprom, seteeprom]=useState(0);
+    const [ram, setram]=useState(0);
+    const [pins, setpins]=useState(0);
+    const [channels, setchannels]=useState(0);
+    const [resolution, setresolution]=useState(0);
+    const [comp, setcomp]=useState(0);
+    const [timer8, settimer8]=useState(0);
+    const [timer16, settimer16]=useState(0);
+    const [serial, setserial]=useState("yes");
+    const [speed, setspeed]=useState(0);
+    const [osc, setosc]=useState(0);
     const [List, setList]= useState([]);
+    const [visible, setvisible]=useState(true);
+
+    const [flip, setflip]=useState(true);
 
     const search = () => {
-        axios.post("http://localhost:3001/user", {name: Pic});
-        axios.get("http://localhost:3001/user").then((response) => {
+        axios.post("http://localhost:3001/search", {name: Pic});
+        axios.get("http://localhost:3001/ressearch").then((response) => {
             setList(response.data)
         }); 
+        if (visible===true) setvisible(!visible);
+    }
+
+    const advance = () => {
+        axios.post("http://localhost:3001/advance",
+            {Memory: memory,
+            Eeprom: eeprom,
+            Ram: ram,
+            Pins: pins,
+            Channels: channels, 
+            Resolution: resolution,
+            Comp: comp,
+            Timer8: timer8,
+            Timer16: timer16,
+            Serial: serial,
+            Speed: speed,
+            Osc: osc      
+        });
+        axios.get("http://localhost:3001/resadvance").then((response) => {
+            setList(response.data)
+        }); 
+        if (visible===true) setvisible(!visible);
+    }
+
+    const change = () => {
+        setflip(!flip);
     }
 
     const rendermicros = (micros) => {
@@ -37,10 +78,68 @@ function App() {
 
     return (
         <div>
-            <input type="number" onChange={(event)=>(setPic(event.target.value))}/>
-            <button onClick={search}>Search</button>
-            <thead>
-                <tr>
+            <div className={flip ? "flip-card" : "flip-card-after"}>
+                <div className={flip ? "flip-card-inner" : "flip-card-inner-after"}>
+                    <div class="flip-card-front">
+                        <input type="text" onChange={(event)=>(setPic(event.target.value))}/>
+                        <button onClick={search}>Search</button><br/><br/>
+                        <button onClick={change} style={{border: "none"}}>Advance Search</button>
+                    </div>
+                    <div class="flip-card-back">
+                    <table>
+                    <tr>
+                        <td style={{border: "none"}}>
+                            <label htmlFor="">Memory:</label><br/>
+                            <input type="text" onChange={(event)=>(setmemory(event.target.value))}/><br/><br/>
+                            <label htmlFor="">EEprom:</label><br/>
+                            <input type="text" onChange={(event)=>(seteeprom(event.target.value))}/><br/><br/>
+                            <label htmlFor="">RAM:</label><br/>
+                            <input type="text" onChange={(event)=>(setram(event.target.value))}/><br/><br/>
+                            <label htmlFor="">I/O Pins:</label><br/>
+                            <input type="text" onChange={(event)=>(setpins(event.target.value))}/><br/><br/>
+                        </td>
+                        <td style={{border: "none"}}> 
+                            <label htmlFor="">ADC</label><br/><br/>
+                            <label htmlFor="">Chanels:</label><br/>
+                            <input type="text" onChange={(event)=>(setchannels(event.target.value))}/><br/>
+                            <label htmlFor="">Resolution:</label><br/>
+                            <input type="text" onChange={(event)=>(setresolution(event.target.value))}/><br/><br/>
+                            <label htmlFor="">Comparator</label><br/>
+                            <input type="radio" name="select" defaultChecked onChange={(event)=>(setcomp(0))}/>
+                            <label htmlFor="">0</label><br/>
+                            <input type="radio" name="select" onChange={(event)=>(setcomp(1))}/>
+                            <label htmlFor="">1</label><br/>
+                            <input type="radio" name="select" onChange={(event)=>(setcomp(2))}/>
+                            <label htmlFor="">2</label><br/>
+                        </td>
+                        <td style={{border: "none"}}>
+                            <label htmlFor="">Timers</label><br/>
+                            <input type="checkbox" onChange={(event)=>(settimer8(1))}/>
+                            <label htmlFor="">8-bit</label><br/>
+                            <input type="checkbox" onChange={(event)=>(settimer16(1))}/>
+                            <label htmlFor="">16-bit</label><br/><br/>
+                            <input type="checkbox" onChange={(event)=>(setserial("no"))}/>
+                            <label htmlFor="">Serial Comm</label><br/><br/>
+                            <label htmlFor="">Max Speed</label><br/>
+                            <input type="text" onChange={(event)=>(setspeed(event.target.value))}/><br/><br/>
+                            <input type="checkbox" onChange={(event)=>(setosc(1))}/>
+                            <label htmlFor="">Internal OSC</label><br/><br/>
+                        </td>
+                    </tr>
+                </table>   
+                <button onClick={advance}>Advance</button> <br/><br/>
+                <button onClick={change} style={{border: "none"}}>Normal Search</button>
+                    </div>
+                </div>
+            </div>
+
+            
+            <div>
+                
+            </div>
+            <div className={visible ? "showoff" : "showon"}>
+            <thead> 
+                <tr >
                     <td>Product</td>
                     <td>Memory</td>
                     <td>Memory_Type</td>
@@ -59,6 +158,7 @@ function App() {
             <tbody>
                 {List.map(rendermicros)}
             </tbody>
+            </div>
         </div>
     );
 }
--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ const app=express();
 app.use(cors());
 app.use(bodyparser.json());
 
-let dato=3;
+var dato;
+var memory;
+var eeprom;
+var ram;
+var pins;
+var channels;
+
 
 const database=mysql.createConnection({
     host:"localhost",
@@ -16,17 +22,39 @@ const database=mysql.createConnection({
     database: "portfolio"
 });
 
+app.post("/search", (req, res) => {
+    dato=req.body.name;
+    dato="%"+dato+"%";
+    res.send()
+});
 
-app.get("/user", (req, res) => {
-    database.query("SELECT * FROM micros, analog, digital, speeds, memorytype, presentation where micro_id=? and micros.micro_id=analog.adc_id and micros.micro_id=digital.dig_id and micros.micro_id=speeds.speed_id and micros.packages=presentation.box_id and micros.memorytype=memorytype.type_id", [dato], (err, result ) => {
+app.get("/ressearch", (req, res) => {
+    database.query("SELECT * FROM micros, analog, digital, speeds, memorytype, presentation where product like ? and micros.micro_id=analog.adc_id and micros.micro_id=digital.dig_id and micros.micro_id=speeds.speed_id and micros.packages=presentation.box_id and micros.memorytype=memorytype.type_id", [dato], (err, result ) => {
         return res.send(result)
     })
 })
 
-app.post("/user", (req, res) => {
-    dato=req.body.name;
+app.post("/advance", (req, res) => {
+    memory=req.body.Memory;
+    eeprom=req.body.Eeprom;
+    ram=req.body.Ram;
+    pins=req.body.Pins;
+    channels=req.body.Channels;
+    resolution=req.body.Resolution;
+    comp=req.body.Comp;
+    timer8=req.body.Timer8;
+    timer16=req.body.Timer16;
+    serial=req.body.Serial;
+    speed=req.body.Speed;
+    osc=req.body.Osc;
     res.send()
 });
+
+app.get("/resadvance", (req, res) => {
+    database.query("SELECT * FROM micros, analog, digital, speeds, memorytype, presentation where micros.micro_id=analog.adc_id and micros.micro_id=digital.dig_id and micros.micro_id=speeds.speed_id and micros.packages=presentation.box_id and micros.memorytype=memorytype.type_id and progmemory>=? and eeprom>=? and ram>=? and pins>=? and ADC>=? and res>=? and comp>=? and timer16>=? and timer8>=? and serial<>? and max>=? and intOSC>=? order by micro_id", [memory, eeprom, ram, pins, channels, resolution, comp, timer16, timer8, serial, speed, osc], (err, result ) => {
+        return res.send(result)
+    })
+})
 
 app.listen(3001, () => {
     console.log("running on port 3001")
